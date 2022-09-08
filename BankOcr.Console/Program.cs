dcbab1e42e7e162bb2f0c386628a9d3bcf3f3179
Alistair Cooke <alistair.cooke77@gmail.com>
2022-09-08 10:13:51 +0100

    Don't read the whole source file
    Stream the line through as required instead.
    
    I thought this was how file.ReadAllLines worked, but noticed that the
    return type meant I must be wrong!

--- BankOcr.Console/Program.cs
+++ BankOcr.Console/Program.cs
@@ -4,7 +4,19 @@
 //we assume success all the way.  It'll do for now!
 
 var sourceFile = args[0];
-var inputLines = File.ReadAllLines(sourceFile);
+
+var inputLines = StreamLines(sourceFile);
 var output = Console.Out;
 
 OcrEntryFile.ProcessInputText(inputLines, output);
+
+static IEnumerable<string> StreamLines(string sourceFile)
+{
+    using var input = File.OpenText(sourceFile);
+    var line = input.ReadLine();
+    while (line != null)
+    {
+        yield return line;
+        line = input.ReadLine();
+    }
+}
