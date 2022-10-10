9bbfb4c8483d6221b389e8d4d90b9c8764bc79ca
Alistair Cooke <alistair.cooke77@gmail.com>
2022-10-10 20:41:56 +0100

    Actually use the exit code in the main program
    Also removes obsolete comment

--- BankOcr.Console/Program.cs
+++ BankOcr.Console/Program.cs
@@ -1,12 +1,9 @@
 ﻿using BankOcr.Console;
 
-//This is missing any validation or proper error handling,
-//we assume success all the way.  It'll do for now!
-
 var program = new BankOcrProgram(
     new FileSystemStreamFinder(),
     Console.Out
 );
 
 var options = ProgramOptions.ParseArgs(args);
-program.Run(options);
+return (int)program.Run(options);
