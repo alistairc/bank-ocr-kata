d542f45cb0a6308fdbc788e5d390dea025edada6
Alistair Cooke <alistair.cooke77@gmail.com>
2022-10-10 20:11:49 +0100

    Arg parsing now validates args
    Invalid args are not yet handled by the rest of the program

--- BankOcr.Console/ProgramOptions.cs
+++ BankOcr.Console/ProgramOptions.cs
@@ -1,9 +1,33 @@
 namespace BankOcr.Console;
 
-public record ProgramOptions(string SourcePath, string? OutputPath)
+public record ProgramOptions
 {
+    public static ProgramOptions Invalid = new();
+
+    public ProgramOptions(string sourcePath, string? outputPath)
+    {
+        SourcePath = sourcePath;
+        OutputPath = outputPath;
+        IsValid = true;
+    }
+
+    ProgramOptions()
+    {
+        IsValid = false;
+        SourcePath = null!;
+        OutputPath = null;
+    }
+
+    public bool IsValid { get; }
+    public string SourcePath { get; }
+    public string? OutputPath { get; }
+
     public static ProgramOptions ParseArgs(string[] commandLineArgs)
     {
+        if (commandLineArgs.Length < 1)
+        {
+            return new ProgramOptions();
+        }
         var sourcePath = commandLineArgs[0];
         var outputPath = commandLineArgs.Length > 1 ? commandLineArgs[1] : null;
         return new ProgramOptions(sourcePath, outputPath);
--- BankOcr.Tests/ParsingCommandLineArgs.cs
+++ BankOcr.Tests/ParsingCommandLineArgs.cs
@@ -8,17 +8,29 @@ public class ParsingCommandLineArgs
     public void TwoArgs_ShouldBeInputAndOutputFiles()
     {
         var parsed = ProgramOptions.ParseArgs(new[] { "first", "second" });
-        
+
+        parsed.IsValid.ShouldBeTrue();
         parsed.SourcePath.ShouldBe("first");
         parsed.OutputPath.ShouldBe("second");
     }
-    
+
     [Test]
     public void OneArg_ShouldBeInputFileOnly()
     {
         var parsed = ProgramOptions.ParseArgs(new[] { "first" });
-        
+
+        parsed.IsValid.ShouldBeTrue();
         parsed.SourcePath.ShouldBe("first");
+        parsed.OutputPath.ShouldBeNull();
+    }
+
+    [Test]
+    public void NoArgs_ShouldBeInvalid()
+    {
+        var parsed = ProgramOptions.ParseArgs(new string[0]);
+
+        parsed.IsValid.ShouldBeFalse();
+        parsed.SourcePath.ShouldBeNull();
         parsed.OutputPath.ShouldBeNull();
     }
 }
