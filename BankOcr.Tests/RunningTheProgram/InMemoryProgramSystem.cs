8dcc5126729be575f871b1fcc5560f05e47b3ed1
Alistair Cooke <alistair.cooke77@gmail.com>
2022-10-12 18:32:51 +0100

    Only test program through public interface
    Drop the tests for ProgramOptions - it's an implementation detail

--- BankOcr.Tests/RunningTheProgram/InMemoryProgramSystem.cs
+++ BankOcr.Tests/RunningTheProgram/InMemoryProgramSystem.cs
@@ -20,7 +20,7 @@ class InMemoryProgramSystem
             StreamFinder.SetupFile("input.txt", Input);
 
             var program = new BankOcrProgram(StreamFinder, StdOut);
-            ExitCode = program.Run(new ProgramOptions("input.txt", "output.txt"));
+            ExitCode = program.Run(ProgramOptions.ParseArgs(new[] {"input.txt", "output.txt"}));
         }
 
         public void RunProgramWithNoOutputFile()
@@ -28,7 +28,7 @@ class InMemoryProgramSystem
             StreamFinder.SetupFile("input.txt", Input);
 
             var program = new BankOcrProgram(StreamFinder, StdOut);
-            ExitCode = program.Run(new ProgramOptions("input.txt", null));
+            ExitCode = program.Run(ProgramOptions.ParseArgs(new[] {"input.txt"}));
         }
 
         public string GetConsoleText()
@@ -44,6 +44,6 @@ class InMemoryProgramSystem
         public void RunProgramWithNoArgs()
         {
             var program = new BankOcrProgram(StreamFinder, StdOut);
-            ExitCode = program.Run(ProgramOptions.Invalid);
+            ExitCode = program.Run(ProgramOptions.ParseArgs(Array.Empty<string>()));
         }
     }
